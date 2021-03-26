52d6d47c242bce1dac72b3112c499c6fbe83aed5
Declan McGuinness <60525697+TheFiftGuy@users.noreply.github.com>
2021-03-26 19:13:45 -0400

    PROG_GameMode_Changes Added FlagHolder sockets to characters, cleaded code from MiniFlag and deleted unused map (Deletion approved by Tremaine)

--- Source/GAME259A/GameMode/MiniFlag.h
+++ Source/GAME259A/GameMode/MiniFlag.h
@@ -15,9 +15,6 @@ class GAME259A_API AMiniFlag : public AFlag
 	GENERATED_BODY()
 
 protected:
-	virtual void PickUp_Implementation(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
-	virtual void Drop_Implementation() override;
 
 	virtual void Capture() override;
 };
