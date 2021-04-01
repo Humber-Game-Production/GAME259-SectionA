4e096284c149fb1dadd244b59d8ca3302cb3281f
TheBenBro <54993958+TheBenBro@users.noreply.github.com>
2021-04-01 15:38:38 -0400

    PROG_M&A_NewFeature_Added Cooldowns for both abilities which can be accessed through BP

--- Source/GAME259A/BaseCharacter.cpp
+++ Source/GAME259A/BaseCharacter.cpp
@@ -7,7 +7,8 @@
 
 // Sets default values
 ABaseCharacter::ABaseCharacter() : MaxWalkSpeed(1200.0f), SprintMultiplier(1.5f), JumpVelocity(800.0f), TeleportThrowLength(1200.0f), TeleportThrowHeight(500.0f),
-									MaxHealth(100.0f), CurrentHealth(MaxHealth), RespawnTime(3.0f), bIsDead(false), bIsSlowed(false),SlowMultiplier(0.25f),bIsStunned(false)
+MaxHealth(100.0f), CurrentHealth(MaxHealth), RespawnTime(3.0f), bIsDead(false), bIsSlowed(false), SlowMultiplier(0.25f), bIsStunned(false), CanUseAbilityOne(true), CanUseAbilityTwo(true),
+AbilityOneCoolDown(8.0f), AbilityTwoCoolDown(12.0f)
 {
 	//Set the character to not rotate when the mouse is moved, only the camera is rotated.
  	bUseControllerRotationPitch = false;
@@ -148,31 +149,39 @@ void ABaseCharacter::MoveForward(float Axis)
 void ABaseCharacter::SetThrowAbilityOne_Implementation()
 {
 	ACTFPlayerState* ctfPlayerState = this->GetPlayerState<ACTFPlayerState>();
-	
-	FVector tmpLoc = FVector(GetActorLocation().X , GetActorLocation().Y, GetActorLocation().Z + 100);
+
+	FVector tmpLoc = FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z + 100);
 	location = FTransform(tmpLoc + GetActorRightVector() * 40.0f);
 	FVector ThrowDistance = ThirdPersonCamera->GetForwardVector() * TeleportThrowLength + ThirdPersonCamera->GetUpVector() * TeleportThrowHeight;
-	if(ACTFPlayerState* StateOfPlayer = GetPlayerState<ACTFPlayerState>())
+	if (ACTFPlayerState* StateOfPlayer = GetPlayerState<ACTFPlayerState>())
 		TeleportAbility->UseAbility(3.0f, location, 0.0f, StateOfPlayer->teamID, 0.0f, ThrowDistance, this);
-	else 
+	else
 		TeleportAbility->UseAbility(3.0f, location, 0.0f, ETeamIdentifier::None, 0.0f, ThrowDistance, this);
 
 	ctfPlayerState->bIsThrowing = false;
-}
+	GetWorld()->GetTimerManager().SetTimer(AbilityOneTimerHandle, [this]()
+		{
+			CanUseAbilityOne = true;
 
+		}, AbilityOneCoolDown, false);
+
+
+}
 void ABaseCharacter::UseAbilityOne_Implementation()
 {
+	if (CanUseAbilityOne) {
 	ACTFPlayerState* ctfPlayerState = this->GetPlayerState<ACTFPlayerState>();
 	//GEngine->AddOnScreenDebugMessage(-1, 3.0f, FColor::Yellow, FString::Printf(TEXT("Can Use Ability In %f"), ForwardVector.X));
 
 	ctfPlayerState->bIsThrowing = true;
 	
-	if (ctfPlayerState->bIsThrowing)
-	{
-		GetWorld()->GetTimerManager().SetTimer(ThrowingTimer, this, &ABaseCharacter::SetThrowAbilityOne,
-
-			1.0f, false);
+		if (ctfPlayerState->bIsThrowing)
+		{
+			GetWorld()->GetTimerManager().SetTimer(ThrowingTimer, this, &ABaseCharacter::SetThrowAbilityOne,1.0f, false);
+			CanUseAbilityOne = false;
+		}
 	}
+	
 }
 
 void ABaseCharacter::SetThrowAbilityTwo_Implementation()
@@ -185,21 +194,28 @@ void ABaseCharacter::SetThrowAbilityTwo_Implementation()
 		SecondAbility->UseAbility(3.0f, location, 0.0f, ETeamIdentifier::None, 0.0f, ThirdPersonCamera->GetForwardVector() * 1000.0f, this);
 
 	ctfPlayerState->bIsThrowing = false;
+	GetWorld()->GetTimerManager().SetTimer(AbilityTwoTimerHandle, [this]()
+		{
+			
+			CanUseAbilityTwo = true;
+		}, AbilityTwoCoolDown, false);
 }
 
 
 void ABaseCharacter::UseAbilityTwo_Implementation()
 {
-	ACTFPlayerState* ctfPlayerState = this->GetPlayerState<ACTFPlayerState>();
-	//TODO
-	//Fill in when the ability class is finished.
-	ctfPlayerState->bIsThrowing = true;
+	if (CanUseAbilityTwo) {
+		ACTFPlayerState* ctfPlayerState = this->GetPlayerState<ACTFPlayerState>();
+		//TODO
+		//Fill in when the ability class is finished.
+		ctfPlayerState->bIsThrowing = true;
 
-	if (ctfPlayerState->bIsThrowing)
-	{
-		GetWorld()->GetTimerManager().SetTimer(ThrowingTimer, this, &ABaseCharacter::SetThrowAbilityTwo,
+		if (ctfPlayerState->bIsThrowing)
+		{
+			GetWorld()->GetTimerManager().SetTimer(ThrowingTimer, this, &ABaseCharacter::SetThrowAbilityTwo,
 
-			1.0f, false);
+				1.0f, false);
+		}
 	}
 }
 
